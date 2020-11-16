066ccc2ee1bc6cd897ccc7fea033d54b37824254
Milton Rodrigues <miltonrodrigues713@gmail.com>
2020-11-16 18:30:55 -0300

    First stage with few CSS styles created.

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+const navbar = document.querySelector(".navbar");
+const menuToggle = document.querySelector(".menu-toggle");
+
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("open");
+});
+
+
+
+/*================================================
+    Nav Button
+=================================================*/
+
+  //Get the Nav Button \\
+const mybutton = document.getElementById("navbutton");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+/*================================================
+    Scrolldown
+=================================================*/
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
